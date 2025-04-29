36808e34877e9762c267dc278a644bc8e82d1e9d
therealdelorian <thenewdorian21@gmail.com>
2025-04-29 13:57:14 -0600

    Suggested changes. Not done yet though, just starting some cleaning code and updating requirements MD file

--- Analysis.py
+++ Analysis.py
@@ -29,96 +29,33 @@ taxi_data.dropna()
 
 # Reshaping data - Creating new columns based on existing data that will help our analysis
 taxi_data['pickup'] = pd.to_datetime(taxi_data['pickup'])
-taxi_data['dropoff'] = pd.to_datetime(taxi_data['dropoff'])
-taxi_data['duration'] = taxi_data['dropoff'] - taxi_data['pickup']
-taxi_data['duration_minutes'] = taxi_data['duration'].dt.total_seconds() / 60
-taxi_data['is_yellow'] = (taxi_data['color'] == 'yellow').astype(int)  
 taxi_data['hour_of_pickup'] = taxi_data['pickup'].dt.hour
-taxi_data['hour_of_dropoff'] = taxi_data['dropoff'].dt.hour
-tips_hour_average = taxi_data.groupby('hour_of_pickup')['tip'].mean()
-print(tips_hour_average)
-
-def time_to_seconds(t):
-    return t.hour * 3600 + t.minute * 60 + t.second
-taxi_data['pickup_seconds'] = taxi_data['pickup'].apply(time_to_seconds)
-taxi_data['dropoff_seconds'] = taxi_data['dropoff'].apply(time_to_seconds)
-
-
-# Exploratory Analysis and Visualizations
-
-# 1
-plt.scatter(taxi_data['pickup_seconds'], taxi_data['dropoff_seconds'])
-plt.xlabel('Pickup Time (seconds since midnight)')
-plt.ylabel('Dropoff Time (seconds since midnight)')
-plt.title('Pickup vs Dropoff Times (in seconds)')
-#plt.show()
-
-
-# hour visualization
+avg_tip_per_ride_per_hr = taxi_data.groupby('hour_of_pickup')['tip'].mean()
 
 # convert to counts,
 hour_counts_pickup = taxi_data['hour_of_pickup'].value_counts().sort_index()
 
-# 2
-sns.barplot(x=hour_counts_pickup.index, y=hour_counts_pickup.values)
+plt.plot(hour_counts_pickup.index, avg_tip_per_ride_per_hr.values)
 plt.xlabel('Hour of Day')
-plt.ylabel('Number of Pickups')
-plt.title('Number of Pickups by Hour')
-#plt.show()
-
-# now for dropoff
-hour_counts_dropoff = taxi_data['hour_of_dropoff'].value_counts().sort_index()
-
-# 2
-sns.barplot(x=hour_counts_dropoff.index, y=hour_counts_dropoff.values)
-plt.xlabel('Hour of Day')
-plt.ylabel('Number of Dropoffs')
-plt.title('Number of Dropoffs by Hour')
-#plt.show()
-
-# 3
-sns.barplot(x=hour_counts_pickup.index, y=tips_hour_average.values)
-plt.xlabel('Hour of Day')
-plt.ylabel('Average Tips')
+plt.ylabel('Average Tip Amount') 
 plt.title('Average Tips by Hour')
+plt.grid(True)
 plt.legend()
-#plt.show()
+plt.show()
 
-# 4
-# the average tips vs hour of day
-hour_average_tips = tips_hour_average.values
-plt.scatter(hour_counts_pickup, hour_average_tips)
-plt.xlabel("#pickups")
-plt.ylabel("Average Tips")
-plt.title("Average tips at time of day vs pickup numbers")
+sns.barplot(x=hour_counts_pickup.index, y=avg_tip_per_ride_per_hr.values)
+plt.xlabel('Hour of Day')
+plt.ylabel('Average Tip Amount')
+plt.title('Average Tips by Hour')
+plt.grid(True)
 plt.legend()
-#plt.show()
+plt.show()
 
 
-# plt.plot(x=hour_counts_pickup.index, y=tips_hour_average.values)
-# plt.xlabel('Hour of Day')
-# plt.ylabel('Average Tips')
-# plt.title('Average tips by Hour of Day')
-# plt.grid(True)
-# plt.show()
+
+
+
 
 
 # Use of at least two analysis techniques such as multiple regression or logistic regression. In
 # addition you should highlight the success of such models and briefly explain various iterations
-X = taxi_data[["distance", 'fare']]
-Y = taxi_data['tip']
-
-X = sm.add_constant(X)
-modelofDurationVTip = sm.OLS(Y, X).fit()
-
-print(modelofDurationVTip.summary())
-
-
-# if not yellow, it's green based off of data
-X= taxi_data['tip']
-Y= taxi_data['is_yellow']
-LogitModelof_Color_to_tip = sm.Logit(Y,X)
-LogitModelof_Color_To_tip_Fit = LogitModelof_Color_to_tip.fit()
-print(LogitModelof_Color_To_tip_Fit.summary())
-odds_ratio = np.exp(LogitModelof_Color_To_tip_Fit.params)
-print(odds_ratio)
--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-# DataScienceFinal_ChipSetAnalysys
-analyze the chip data set from https://chip-dataset.vercel.app/
-
+# Taxi Driving Tips Analysis
 
 # Requirements 
 ### A final-level project in this course should involve:
 - A clearly defined problem or question related to the dataset
    - When is the best time of day to drive to get the most tips?
-   - Can we predict tip amount based on _____ certain other variables
-   - What factors influence total fare? (to try and find out how to lower them?)
-   - predict payment type based on trip characteristics?
+   - Can we predict tip amount based on time of day or other variables?
 - Data acquisition (e.g., scraping/API, or strong justification for choosing pre-cleaned open data)
-   - hopefully we can get it from an api/web scrape
-   - from the csv came from their download button, but
-    if you look at the websites console, it includes
-    all the data why? nobody knows
+   - We get our data on Kagglehub. enocknkuya/tax-dropoff
 - Cleaning and reshaping of the data (with code included)
-   - clean to remove the nans, and tranform some things
+   - We will remove nas, and we will create new columns with new data calculated from the existing data, such as the hour of day as discrete values, and the average tip amount for each hour.
 - Exploratory analysis and visualizations. (At least 4 visualizations)
-   - Graphs that show  the correlations and how we can use those to predict what we want to predict.
+   1. Line chart that shows time of day vs average tip amount per ride.
+   2. Bar chart for #1 to easily distinguish the hours of the day since we made those values discrete.
+   3. Line chart showing the number of rides per hour.
 - Use of at least two analysis techniques such as multiple regression or logistic regression. In
 addition you should highlight the success of such models and briefly explain various iterations.
 - Communication of results using plots, tables in a well-organized report. The report should over
